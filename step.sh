8bd9a03b8df52609bd1caa35761cfc798ea1da68
moatazeldebsy <moataznabil2019@gmail.com>
2022-11-13 21:07:23 +0100

    update the step.sh file

--- step.sh
+++ step.sh
@@ -14,5 +14,9 @@ saucectl configure --username $SAUCE_USERNAME --accessKey $SAUCE_ACCESS_KEY
 
 echo "starting uploading the app and test app to Saucelabs and running the automated tests"
 
-
-saucectl run --config $SAUCE_CONFIG_FILE_PATH
+if [ "$SAUCE_CONFIG_FILE_PATH" == "" ]; 
+then
+	saucectl run --config .sauce/config.yml
+else
+	saucectl run --config $SAUCE_CONFIG_FILE_PATH
+fi
